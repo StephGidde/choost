198615ce930cb03442e6485bd64b0ea08ea4fd2c
Axel Heinz <axelmheinz@gmail.com>
2019-03-07 21:12:47 +0100

    server setup auth routes user model auth-component

---
+++ client/src/components/auth/auth-service.js
@@ -0,0 +1,15 @@
+// auth/auth-service.js
+
+import axios from "axios";
+
+class AuthService {
+  constructor() {
+    let service = axios.create({
+      baseURL: "http://localhost:5000/api",
+      withCredentials: true
+    });
+    this.service = service;
+  }
+}
+
+export default AuthService;
--- server/app.js
+++ server/app.js
@@ -1,58 +1,89 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
+  .connect("mongodb://localhost/choost-db", { useNewUrlParser: true })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
+// ADD SESSION SETTINGS
+app.use(
+  session({
+    secret: "s3Cr3T",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+    // Mongostore => damit man nach Server-Neustart nicht ausgeloggt wird
+  })
+);
 
+// USE passport.initialize() and passport.session() HERE:
+app.use(passport.initialize());
+app.use(passport.session());
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Choost - MERN Project by SG SD AMH";
 
+// ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
+const cors = require("cors");
 
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"] // <== this will be the URL of our React app (it will be running on port 3000)
+  })
+);
 
-const index = require('./routes/index');
-app.use('/', index);
+// api/signup
+const authRoutes = require("./routes/auth-routes");
+app.use("/api", authRoutes);
 
+const index = require("./routes/index");
+app.use("/", index);
 
 module.exports = app;
