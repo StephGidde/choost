4a2f5d3c35cb4ee716895b8bde3908722a4999c3
Axel Heinz <axelmheinz@gmail.com>
2019-03-30 15:00:43 +0100

    app.js added stuff

--- server/app.js
+++ server/app.js
@@ -122,4 +122,8 @@ app.use("/api", authRoutes);
 const index = require("./routes/index");
 app.use("/", index);
 
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
 module.exports = app;
