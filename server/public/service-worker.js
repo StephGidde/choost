f9295ed23211eeab81124c160edcef6e424e06f3
Axel Heinz <axelmheinz@gmail.com>
2019-04-02 22:09:45 +0200

    configured build

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ecff31430a97c6ed5d14893e557319f.js"
+  "/precache-manifest.31b2c502b4623abfa15848ab06ec20d6.js"
 );
 
 workbox.clientsClaim();
