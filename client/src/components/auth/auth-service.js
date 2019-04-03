e558a9b092abee20061f8f7e1589cb66c6070d1f
Axel Heinz <axelmheinz@gmail.com>
2019-04-03 14:47:28 +0200

    comma fix in auth-services

--- client/src/components/auth/auth-service.js
+++ client/src/components/auth/auth-service.js
@@ -6,7 +6,7 @@ class AuthService {
   constructor() {
     let service = axios.create({
       baseURL:
-      (process.env.REACT_APP_API_URL || "http://localhost:5000/")  + "api"
+        (process.env.REACT_APP_API_URL || "http://localhost:5000/") + "api",
       withCredentials: true
     });
     this.service = service;
